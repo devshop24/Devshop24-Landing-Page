57e6ea9153af03d56db0e4983ead1ab8e9ad95a8
Jen Chartier <54649817+JChartier001@users.noreply.github.com>
2020-06-01 22:56:41 -0500

    commented out index.js and added info to server.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const server = require( './server' );
+// const server = require( './server' );
 
-const PORT = process.env.PORT || '5000';
+// const PORT = process.env.PORT || '5000';
 
-server.listen(PORT, () => {
-    console.log(
-        `\n************************************************************
-         \n\t\tServer Running on port:${PORT}
-         \n************************************************************`
-    )
-});
+// server.listen(PORT, () => {
+//     console.log(
+//         `\n************************************************************
+//          \n\t\tServer Running on port:${PORT}
+//          \n************************************************************`
+//     )
+// });
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const server = require( 'express' )();
 const routes = require( './routes' );
+import app from "./app.js"
+import config from './config';
 
+const { PORT } = config;
+
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
 
 // TODO - import middleware
 
