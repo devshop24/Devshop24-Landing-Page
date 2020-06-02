53988fed5fc5557c54ad42122adb16f8336fb84e
Jen Chartier <54649817+JChartier001@users.noreply.github.com>
2020-06-01 22:37:36 -0500

    subscriber model

---
+++ models/subscribers.js
@@ -0,0 +1,24 @@
+import { Schema, model } from 'mongoose';
+
+// Create Schema
+const SubscribersSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+      type: String,
+      require: true
+  },
+  subscribed: {
+      type: Boolean,
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Subscribers = model('subscribers', SubscribersSchema);
+
+export default Subscribers;
